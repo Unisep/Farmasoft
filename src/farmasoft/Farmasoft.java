e4df088766ea852c1e3d07dea9fea9a2575f1ca9
Luiz Carraro <lacarraro@gmail.com>
2015-10-17 08:25:36 -0300

    Alteração de comentários da classe principal

--- src/farmasoft/Farmasoft.java
+++ src/farmasoft/Farmasoft.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Clase principal do programa
  */
 package farmasoft;
 
@@ -15,7 +13,6 @@ public class Farmasoft {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        System.out.println("Hello World");
+    } 
 }
